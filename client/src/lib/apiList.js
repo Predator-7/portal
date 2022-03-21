51b2e243a71c79ab71690b2fa1bfb42fe69b6485
unknown <anshgarewal7@gmail.com>
2022-03-21 15:18:01 +0530

    pp

--- client/src/lib/apiList.js
+++ client/src/lib/apiList.js
@@ -1,15 +1,15 @@
 export const server = "http://localhost:4444";
 
 const apiList = {
-  login: `${server}/auth/login`,
-  signup: `${server}/auth/signup`,
-  uploadResume: `${server}/upload/resume`,
-  uploadProfileImage: `${server}/upload/profile`,
-  jobs: `${server}/api/jobs`,
-  applications: `${server}/api/applications`,
-  rating: `${server}/api/rating`,
-  user: `${server}/api/user`,
-  applicants: `${server}/api/applicants`,
+    login: `/auth/login`,
+    signup: `/auth/signup`,
+    uploadResume: `/upload/resume`,
+    uploadProfileImage: `/upload/profile`,
+    jobs: `/api/jobs`,
+    applications: `/api/applications`,
+    rating: `/api/rating`,
+    user: `/api/user`,
+    applicants: `/api/applicants`,
 };
 
 export default apiList;
